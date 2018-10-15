e7ee08d8eb109d59bcc2e350e3f09007bd4fc978
Suhwan Jee <suhwan.jee@lablup.com>
2018-10-15 16:44:59 +0900

    Use alembic for DB migration

--- server/src/models.py
+++ server/src/models.py
@@ -1,10 +1,13 @@
 import sqlalchemy as sa
 
 
+metadata = sa.MetaData()
+
+
 user = sa.Table(
-    'user', sa.MetaData(),
+    'user', metadata,
     sa.Column('row_id', sa.Integer, primary_key=True),
     sa.Column('id', sa.String(length=30), unique=True),
-    sa.Column('nickname', sa.String(length=30), unique=True),
-    sa.Column('password', sa.String(length=30)),
+    sa.Column('nickname', sa.String(length=32), unique=True),
+    sa.Column('password', sa.String(length=32)),
 )
