8a828b993809a9106cba141df23aba9800dcb871
Suhwan Jee <suhwan.jee@lablup.com>
2018-10-17 21:39:06 +0900

    Refactor fields out into HorizontalInputField component

---
+++ client/src/components/HorizontalInputField.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="field is-horizontal">
+    <div class="field-label is-normal">
+      <slot name="label"></slot>
+    </div>
+    <div class="field-body">
+      <div class="control">
+        <slot name="input"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'horizontal-input-field',
+};
+</script>
+
+<style scoped>
+
+</style>
--- client/src/views/Buy.vue
+++ client/src/views/Buy.vue
@@ -2,52 +2,29 @@
   <div>
     <h2 class="title">사과즙 구매하기</h2>
     <p class="subtitle">사과즙을 구매하시려면 아래의 정보를 입력해주세요!</p>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label" for="sender-name">보내시는 분 성함</label>
+    <horizontal-input-field>
+      <label slot="label" class="label" for="sender-name">보내시는 분 성함</label>
+      <input slot="input" id="sender-name" class="input"
+             type="text" v-model="senderName" />
+    </horizontal-input-field>
+    <horizontal-input-field>
+      <label slot="label" class="label" for="applejuice-number">주문 수량</label>
+      <div slot="input" class="select">
+        <select id="applejuice-number" v-model="applejuiceNum">
+          <option v-for="num in range(10)" :key="num">{{ num }}</option>
+        </select>
       </div>
-      <div class="field-body">
-        <div class="control">
-          <input id="sender-name" class="input" type="text" v-model="senderName" />
-        </div>
-      </div>
-    </div>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label" for="applejuice-number">주문 수량</label>
-      </div>
-      <div class="field-body">
-        <div class="control">
-          <div class="select">
-            <select id="applejuice-number" v-model="applejuiceNum">
-              <option v-for="num in range(10)" :key="num">
-                {{ num }}
-              </option>
-            </select>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="field is-horizontal">
-      <div class="field-label">
-        <label class="label" for="receiver-name">받으시는 분 성함</label>
-      </div>
-      <div class="field-body">
-        <div class="control">
-          <input id="receiver-name" class="input" type="text" v-model="receiverName" />
-        </div>
-      </div>
-    </div>
-    <div class="field is-horizontal">
-      <div class="field-label">
-        <label class="label" for="receiver-address">받으시는 분 주소</label>
-      </div>
-      <div class="field-body">
-        <div class="control">
-          <input id="receiver-address" class="input" type="text" v-model="receiverAddress"/>
-        </div>
-      </div>
-    </div>
+    </horizontal-input-field>
+    <horizontal-input-field>
+      <label slot="label" class="label" for="receiver-name">받으시는 분 성함</label>
+      <input slot="input" id="receiver-name" class="input"
+             type="text" v-model="receiverName" />
+    </horizontal-input-field>
+    <horizontal-input-field>
+      <label slot="label" class="label" for="receiver-address">받으시는 분 주소</label>
+      <input slot="input" id="receiver-address" class="input"
+             type="text" v-model="receiverAddress" />
+    </horizontal-input-field>
     <div class="container">
       <button class="button is-primary">Submit</button>
     </div>
@@ -56,6 +33,7 @@
 
 <script>
 import { range } from '../utils/number'; /* eslint-disable-line */
+import HorizontalInputField from '../components/HorizontalInputField';
 
 export default {
   name: 'Buy',
@@ -71,6 +49,9 @@ export default {
     range(n) {
       return range(n);
     },
+  },
+  components: {
+    HorizontalInputField,
   },
 };
 </script>
