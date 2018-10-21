eb35c34c0aed89d2aee468041b7119b5a7c0bfb9
Suhwan Jee <suhwan.jee@lablup.com>
2018-10-21 13:57:10 +0900

    Update eslint config

--- client/src/utils/number.js
+++ client/src/utils/number.js
@@ -4,6 +4,6 @@
  * @param n Length of generated range array
  * @returns {number[]} Array from 0 to n - 1 ([0, 1, ..., n-1])
  */
-export function range(n) { /* eslint-disable-line */
+export function range(n) {
   return [...Array(n).keys()];
 }
