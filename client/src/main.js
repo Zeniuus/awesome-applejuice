9f76ea6bed9286ff091c19a2b42432643b2527c5
Suhwan Jee <suhwan.jee@lablup.com>
2018-11-01 15:11:14 +0900

    Set Authorization headers for each request automatically

--- client/src/main.js
+++ client/src/main.js
@@ -9,6 +9,13 @@ import bulma from './main.scss'; /* eslint-disable-line */
 
 Vue.config.productionTip = false;
 
+axios.interceptors.request.use((config) => {
+  if (store.state.jwt) {
+    /* eslint-disable-next-line no-param-reassign, dot-notation */
+    config.headers['Authorization'] = `Bearer ${store.state.jwt}`;
+  }
+  return config;
+});
 Vue.prototype.$http = axios;
 
 new Vue({
